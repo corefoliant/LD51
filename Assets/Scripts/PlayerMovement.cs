559653c54a625a87686caf5a524ba619f2380b6f
Cucmbr <vanvanich531@gmail.com>
2022-10-01 19:06:42 +0300

    Player Controller
    Models, movement, animations, camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+
+    [SerializeField] private float speed;
+
+    private Vector3 velocity;
+
+    [SerializeField] private float turnSmoothTime;
+    private float turnSmoothVelocity;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float jumpHeight;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+                anim.SetTrigger("JumpTrigger");
+            }
+        }
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+
+            anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+}
