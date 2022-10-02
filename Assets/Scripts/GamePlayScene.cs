68d62b7ef454d1fb745876d1df40c940b120fef4
YanKonopelko <komand47@mail.ru>
2022-10-02 19:57:43 +0200

    GameOver

--- Assets/Scripts/GamePlayScene.cs
+++ Assets/Scripts/GamePlayScene.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GamePlayScene : MonoBehaviour
 {
+    [SerializeField] private GameObject DeathPanel;
+
+    public void Death()
+    {
+        DeathPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    
     public void ExitToMenu()
     {
         SceneManager.LoadScene(1);
