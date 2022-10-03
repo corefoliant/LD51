ff92c23d8dc92aac3943948ebd29db14c18f35d3
DrachenClon22 <30703852+DrachenClon22@users.noreply.github.com>
2022-10-03 20:59:25 +0300

    титры

--- Assets/Scripts/Menu Buttons/CreditsMenuButtons.cs
+++ Assets/Scripts/Menu Buttons/CreditsMenuButtons.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,12 +10,34 @@ public class CreditsMenuButtons : MonoBehaviour
     public Canvas _mainMenuCanvas;
     public Canvas _creditsCanvas;
     [Space]
-    public GameObject _textCredits;
+    public TMPro.TMP_Text _textCredits;
+    [Space]
+    public string[] _credits;
+    [Space]
+    public float _secondsToWait = 3f;
+
+    private IEnumerator _changeCredits;
 
     public void Back_Button()
     {
         _settingsCanvas.gameObject.SetActive(false);
         _mainMenuCanvas.gameObject.SetActive(true);
         _creditsCanvas.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        if (_changeCredits != null)
+            StopCoroutine(_changeCredits);
+        StartCoroutine(_changeCredits = ChangeCredits());
+    }
+
+    private IEnumerator ChangeCredits()
+    {
+        for (int i = 0; i < _credits.Length; i++)
+        {
+            yield return new WaitForSeconds(_secondsToWait);
+            _textCredits.text = _credits[i];
+        }
     }
 }
